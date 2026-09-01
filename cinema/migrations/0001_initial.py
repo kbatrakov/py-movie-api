c2520e1b194a9c428897573cd54b740d71647daf
Kyrylo Batrakov <kirillbatrakov0819@gmail.com>
2026-09-01 19:24:56 +0200

    Solution

--- cinema/migrations/0001_initial.py
+++ cinema/migrations/0001_initial.py
@@ -7,20 +7,27 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Movie',
+            name="Movie",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=256)),
-                ('description', models.TextField(blank=True, max_length=1000)),
-                ('duration', models.IntegerField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=256)),
+                ("description", models.TextField(blank=True, max_length=1000)),
+                ("duration", models.IntegerField()),
             ],
             options={
-                'verbose_name_plural': 'movies',
+                "verbose_name_plural": "movies",
             },
         ),
     ]
--- cinema/migrations/0002_alter_movie_description.py
+++ cinema/migrations/0002_alter_movie_description.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cinema', '0001_initial'),
+        ("cinema", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='movie',
-            name='description',
+            model_name="movie",
+            name="description",
             field=models.CharField(blank=True, max_length=1000),
         ),
     ]
